6c1080587e3fd97895862b86c23e0ab74419271d
Galla Niranjan <135509519+zeusXtruealpha@users.noreply.github.com>
2025-11-28 16:33:07 +0530

    Updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs, query, where } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
-import { getAnalytics, isSupported } from "firebase/analytics"; // Import isSupported
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-//Firebase credentials
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Firestore for event storage
-const auth = getAuth(app); // Firebase Authentication
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Initialize Analytics only if supported (browser environment)
+// Analytics only if supported
 let analytics;
 isSupported().then((supported) => {
-  if (supported) {
-    analytics = getAnalytics(app);
-  }
+  if (supported) analytics = getAnalytics(app);
 });
 
 export {
@@ -40,5 +37,5 @@ export {
   signOut,
   sendEmailVerification,
   sendPasswordResetEmail,
-  analytics, // Export analytics if needed elsewhere
+  analytics,
 };
