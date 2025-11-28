7409637834132b8f56b8b3241a706a2c2d0c4dbb
Galla Niranjan <135509519+zeusXtruealpha@users.noreply.github.com>
2025-11-28 16:55:43 +0530

    Update api key

--- src/WasteCollectionBooking.jsx
+++ src/WasteCollectionBooking.jsx
@@ -168,7 +168,7 @@ const WasteCollectionBooking = () => {
             </button>
           </div>
           <div className="map-container">
-            <LoadScript googleMapsApiKey="">// place your API key here
+            <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
               <GoogleMap
                 mapContainerStyle={{ width: "100%", height: "100%" }}
                 zoom={10}
